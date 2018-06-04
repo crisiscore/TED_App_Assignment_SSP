638d8df7c997cc4ba0106fea98b2427b35c58902
Crisis Core <crisiscore.pai93@gmail.com>
2018-06-04 23:09:54 +0630

    added watch next list

--- app/src/main/java/xyz/traver/tedtalks/activities/TalkDetailsActivity.java
+++ app/src/main/java/xyz/traver/tedtalks/activities/TalkDetailsActivity.java
@@ -2,11 +2,14 @@ package xyz.traver.tedtalks.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import xyz.traver.tedtalks.R;
+import xyz.traver.tedtalks.adapters.WatchNextAdapter;
 
 public class TalkDetailsActivity extends BaseActivity {
 
@@ -19,6 +22,10 @@ public class TalkDetailsActivity extends BaseActivity {
         if (getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        RecyclerView watchNext = findViewById(R.id.rv_watch_next);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext() , LinearLayoutManager.VERTICAL , false);
+        watchNext.setLayoutManager(linearLayoutManager);
+        watchNext.setAdapter(new WatchNextAdapter());
     }
 
     @Override
---
+++ app/src/main/java/xyz/traver/tedtalks/adapters/WatchNextAdapter.java
@@ -0,0 +1,29 @@
+package xyz.traver.tedtalks.adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import xyz.traver.tedtalks.R;
+import xyz.traver.tedtalks.viewholders.WatchNextViewHolder;
+
+public class WatchNextAdapter extends RecyclerView.Adapter {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_holder_watch_next , parent , false);
+        return new WatchNextViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
